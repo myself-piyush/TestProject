f76c7046f02db55cf0283a7de8b82958857f0bc5
MyselfPiyush <62210260+myself-piyush@users.noreply.github.com>
2022-05-08 11:49:48 +0530

    Added player, door, fake door png files, added on trigger event and modified player movement(as per Bonus part)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,13 +4,25 @@ public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D rb2d;
     private float verticalInput, horizontalInput;
-    public int speed = 5;
+    public int speed;
     void Update()
     {
         verticalInput = Input.GetAxis("Vertical");
         horizontalInput = Input.GetAxis("Horizontal");
-
-        rb2d.velocity = new Vector2(horizontalInput * speed, verticalInput * speed);
-        
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb2d.velocity = new Vector2(0, 0);
+        }
+        else
+        {
+            rb2d.velocity = new Vector2(horizontalInput * speed * Time.deltaTime, verticalInput * speed * Time.deltaTime);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Door")
+        {
+            Debug.Log("How You Doin!");
+        }
     }
 }
