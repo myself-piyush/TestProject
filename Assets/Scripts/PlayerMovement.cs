181fba072b2a352650cc421db27758b3442da1d4
MyselfPiyush <62210260+myself-piyush@users.noreply.github.com>
2022-05-08 11:05:24 +0530

    Added Player Movements

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb2d;
+    private float verticalInput, horizontalInput;
+    public int speed = 5;
+    void Update()
+    {
+        verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        rb2d.velocity = new Vector2(horizontalInput * speed, verticalInput * speed);
+        
+    }
+}
