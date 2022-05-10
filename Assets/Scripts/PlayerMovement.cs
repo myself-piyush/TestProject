f45fbfb6ff7a1d9bc665fb822447749edfe32806
MyselfPiyush <62210260+myself-piyush@users.noreply.github.com>
2022-05-10 11:23:24 +0530

    Added Game Won UI and functionality

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,24 +5,30 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody2D rb2d;
     private float verticalInput, horizontalInput;
     public int speed;
+    public GameObject GameWon;
+    private bool isGameOver = false;
     void Update()
     {
-        verticalInput = Input.GetAxis("Vertical");
-        horizontalInput = Input.GetAxis("Horizontal");
-        if (Input.GetKey(KeyCode.Space))
+        if (!isGameOver)
         {
-            rb2d.velocity = new Vector2(0, 0);
-        }
-        else
-        {
-            rb2d.velocity = new Vector2(horizontalInput * speed * Time.deltaTime, verticalInput * speed * Time.deltaTime);
+            verticalInput = Input.GetAxis("Vertical");
+            horizontalInput = Input.GetAxis("Horizontal");
+            if (Input.GetKey(KeyCode.Space))
+            {
+                rb2d.velocity = new Vector2(0, 0);
+            }
+            else
+            {
+                rb2d.velocity = new Vector2(horizontalInput * speed * Time.deltaTime, verticalInput * speed * Time.deltaTime);
+            }
         }
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Door")
         {
-            Debug.Log("How You Doin!");
+            GameWon.SetActive(true);
+            isGameOver = true;
         }
     }
 }
